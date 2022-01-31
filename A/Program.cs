e5f3cb61c61fdb15d7c00c51415daab9058336b8
Harsh <hp7075161@gmail.com>
2022-01-31 19:48:02 +0530

    Final Stages

---
+++ A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class MainClass
+    {
+        public static void Main(string[] argds)
+        {
+            int num, deno;
+
+            Console.WriteLine("Enter 1--> ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2 --> ");
+            deno = int.Parse(Console.ReadLine());
+
+
+            int quotient = num / deno;
+            int reminder = num % deno;
+
+            Console.WriteLine($"Quotient --> {quotient} , Reminder --> {reminder}");
+
+            double fra_quotient = Math.Round((double)num / deno);
+
+            Console.WriteLine($"Fractional Qoutient --> {fra_quotient}");
+            Console.WriteLine($"Mixes Fraction --> {quotient} {reminder}/{deno}");
+        }
+    }
+}
